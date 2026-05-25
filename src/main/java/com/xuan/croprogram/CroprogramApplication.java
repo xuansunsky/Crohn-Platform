208111a71bcec95a9c4517b537dc3146ba7a8b1a
xuansunsky <2500920476@qq.com>
2026-05-25 08:56:48 +0800

    feat: 重构上传逻辑，彻底粉碎带秘钥的脏历史

--- src/main/java/com/xuan/croprogram/CroprogramApplication.java
+++ src/main/java/com/xuan/croprogram/CroprogramApplication.java
@@ -1,14 +1,10 @@
 package com.xuan.croprogram;
 
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 @SpringBootApplication
 public class CroprogramApplication {
 
